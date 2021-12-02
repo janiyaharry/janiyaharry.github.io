f1db934397a1d5631b998550369175fbe0b61b4f
janiyaharry <janiyaharry06@gmail.com>
2021-12-02 15:11:52 +0000

    rearranged platfroms

---
+++ projects/platformer/js/init/platform.js
@@ -0,0 +1,53 @@
+(function (window) {
+    'use strict';
+    window.opspark = window.opspark || {};
+    window.opspark.platform = window.opspark.platform || {};
+    
+    let platform = window.opspark.platform;
+    
+    /**
+     * init: This function initializes the platforms for the level.
+     * 
+     * GOAL: Add as many platforms necessary to make your level challenging.
+     * 
+     * Use the createPlatform Function to create platforms for the level. 
+     * 
+     * createPlatform() takes these arguments:
+     *      
+     *      createPlatform(x, y, scaleX, scaleY);
+     * 
+     *      x: The x coordineate for the platform.
+     *      y: The y coordineate for the platform.
+     *      scaleX: OPTIONAL The scale factor on the x-axis, this value will 
+     *              stretch the platform in width.
+     *      scaleY: OPTIONAL The scale factor on the y-axis, this value will 
+     *              stretch the platform in height.
+     */ 
+    function init(game) {
+        let createPlatform = platform.create;
+
+        ////////////////////////////////////////////////////////////////////////
+        // ALL YOUR CODE GOES BELOW HERE ///////////////////////////////////////
+        
+        /*
+         * ground : here, we create a floor. Given the width of of the platform 
+         * asset, giving it a scaleX and scaleY of 2 will stretch it across the 
+         * bottom of the game.
+         */
+        createPlatform(0, game.world.height - 32, 3, 2);    // DO NOT DELETE
+
+        // example:
+        createPlatform(360, 250, 0.6, 1); // Top Platfrom
+        createPlatform(550, 590, 0.7); // Bottom Platform
+        createPlatform(820, 490, 0.2); // Second Platform
+        createPlatform(280, 440, 0.8, 1,); // Third Platform
+        createPlatform(0, 360, 0.3, 1,); // Fourth Platform
+        createPlatform(0, 150, 0.6, 1,); // Fith Platform
+        
+        
+        
+        // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
+        ////////////////////////////////////////////////////////////////////////
+    }
+    platform.init = init;
+})(window);
